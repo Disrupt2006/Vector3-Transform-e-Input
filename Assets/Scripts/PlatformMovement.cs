b1eefc69f73d2c8805a9396562cc31d5c0ce356a
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-05-23 14:09:42 -0300

    Plataformas terminadas

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -11,6 +11,9 @@ public class PlatformMovement : MonoBehaviour
     public GameObject prefab1;
     public GameObject prefab2;
 
+    public GameObject BaseDer;
+    public GameObject BaseIzq;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,15 +32,19 @@ public class PlatformMovement : MonoBehaviour
         {
             transform.position -= new Vector3(xSpeed, 0, 0);
         }
-        if (transform.position.x >= -4)
+        if (transform.position.x <= -4)
         {
-            toRight = false;
-            Instantiate(prefab1);
+            toRight = true;
+            GameObject clon;
+            clon = Instantiate(prefab1);
+            clon.transform.position = BaseIzq.transform.position + new Vector3(0, 1, 0);
         }
         if (transform.position.x >= 4)
         {
-            toRight = true;
-            Instantiate(prefab2);
+            toRight = false;
+            GameObject clon;
+            clon = Instantiate(prefab2);
+            clon.transform.position = BaseDer.transform.position + Vector3.up;
         }
         
     }
